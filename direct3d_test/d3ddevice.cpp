688dd2e243e25efb112fae3ec523c4c09c7e25d4
cai <caiyax@qq.com>
2019-06-23 16:44:39 +0800

    clear screen

--- direct3d_test/d3ddevice.cpp
+++ direct3d_test/d3ddevice.cpp
@@ -16,7 +16,8 @@ bool Direct3dDevice::init(HWND hwnd)
 	m_D3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
 	m_D3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
 
-	auto ret = m_IDevice->CreateDevice(0, D3DDEVTYPE_HAL, m_Hwnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &m_D3dpp, &m_Device);
+	auto ret = m_IDevice->CreateDevice(0, D3DDEVTYPE_HAL, m_Hwnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &m_D3dpp,
+	                                   &m_Device);
 	if (FAILED(ret))
 	{
 		printf("create d3d failed\n");
@@ -27,9 +28,15 @@ bool Direct3dDevice::init(HWND hwnd)
 
 void Direct3dDevice::render()
 {
-	m_Device->Clear(0, nullptr, D3DCLEAR_TARGET | D3DCLEAR_STENCIL | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0xff, 0x44, 0x88),
-	                1.0, 0);
-	m_Device->BeginScene();
-	m_Device->EndScene();
-	m_Device->Present(nullptr, nullptr, nullptr, nullptr);
+	HRESULT ret;
+	ret = m_Device->Clear(D3DADAPTER_DEFAULT, nullptr, D3DCLEAR_TARGET ,
+	                      D3DCOLOR_XRGB(0x22, 0x44, 0x88),
+	                      1.0, 0);
+	if(FAILED(ret))
+	{
+		return;
+	}
+	ret = m_Device->BeginScene();
+	ret = m_Device->EndScene();
+	ret = m_Device->Present(nullptr, nullptr, nullptr, nullptr);
 }

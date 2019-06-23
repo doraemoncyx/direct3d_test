023a3537095e0d61ee793286987359dd35687a52
cai <caiyax@qq.com>
2019-06-23 16:32:02 +0800

    1

---
+++ direct3d_test/d3ddevice.cpp
@@ -0,0 +1,35 @@
+#include "d3ddevice.h"
+#include <cstdio>
+
+#pragma comment(lib, "d3d9.lib")
+
+Direct3dDevice::Direct3dDevice(HWND hwnd):
+	m_Hwnd(hwnd)
+{
+}
+
+bool Direct3dDevice::init()
+{
+	m_IDevice = Direct3DCreate9(D3D_SDK_VERSION);
+	ZeroMemory(&m_D3dpp, sizeof(m_D3dpp));
+	m_D3dpp.Windowed = true;
+	m_D3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	m_D3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+
+	auto ret = m_IDevice->CreateDevice(0, D3DDEVTYPE_HAL, m_Hwnd, 0, &m_D3dpp, &m_Device);
+	if (FAILED(ret))
+	{
+		printf("create d3d failed\n");
+		return false;
+	}
+	return true;
+}
+
+void Direct3dDevice::render()
+{
+	m_Device->Clear(0, nullptr, D3DCLEAR_TARGET | D3DCLEAR_STENCIL | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0x22, 0x44, 0x88),
+	                1.0, 0);
+	m_Device->BeginScene();
+	m_Device->EndScene();
+	m_Device->Present(nullptr, nullptr, nullptr, nullptr);
+}
---
+++ direct3d_test/d3ddevice.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <d3d9.h>
+
+class Direct3dDevice
+{
+public:
+	Direct3dDevice(HWND hwnd);
+	bool init();
+	void render();
+
+private:
+	IDirect3D9 *m_IDevice;
+	D3DPRESENT_PARAMETERS m_D3dpp;
+	IDirect3DDevice9* m_Device;
+	HWND m_Hwnd;
+};
